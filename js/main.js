7c2303f5708e4c7b2e0566fd632ba11fd3857e78
amiecollins <a_collins84448@fanshaweonline.ca>
2019-01-25 13:18:41 -0500

    svg error, clipping masks conflicting

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
 (() => {
-	// self-invoking anonymous function (or Lamba)
-	var images = document.querySelectorAll('img');
 
-	console.log(images);
+	var allTheIcons = document.querySelectorAll('.isanicon');
 
+	function logMyId() {
+		// log some stuff to the dev tools
+		console.log(this.id);
+	}
+
+	allTheIcons.forEach(currentIcon => currentIcon.addEventListener('click', logMyId));
 })();
+
+
